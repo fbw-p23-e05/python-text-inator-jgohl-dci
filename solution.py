102469a59f785cd914a7a43e11fea2b62a54dd8d
jgohl-dci <johannes.gohl@dci-student.org>
2023-08-31 14:21:24 +0200

    done

---
+++ solution.py
@@ -0,0 +1,12 @@
+user_input = input("Give me a word: ")
+user_input = user_input.capitalize()
+consonant = "a, e, u, o"
+
+if user_input[-1] in consonant:
+    length = len(user_input)
+    user_input += "-inator " + str(length) + "000"
+else:
+    length = len(user_input)
+    user_input += "inator " + str(length) + "000"
+
+print(user_input)
