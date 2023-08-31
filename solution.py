094c0e0203b2556ad8d8edf5c285128aa56b0bd0
jgohl-dci <johannes.gohl@dci-student.org>
2023-08-31 16:01:00 +0200

    add i

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 user_input = input("Give me a word: ")
 user_input = user_input.capitalize()
-consonant = "a, e, u, o"
+consonant = "a, e, u, o, i"
 
 if user_input[-1] in consonant:
     length = len(user_input)
